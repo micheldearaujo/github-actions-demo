b7eaaaf1e5124b85bbe8f04412affda4fa98e47c
Michel de Araujo <78417934+micheldearaujo@users.noreply.github.com>
2023-01-29 01:26:54 +0000

    fixing input function

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 from hello import add, multiply
 
-number1 = float("Type number 1")
-number2 = float("Type number 2")
+number1 = float(input("Type number 1: "))
+number2 = float(input("Type number 2: "))
 
 print(f"Adding {number1} + {number2}...")
 print(f"The result is: {add(number1, number2)}")
