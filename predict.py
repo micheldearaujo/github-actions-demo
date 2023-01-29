a2da876f04fce49d23885d7ac539bef8a23d9f35
Michel de Araujo <78417934+micheldearaujo@users.noreply.github.com>
2023-01-29 01:10:34 +0000

    added a simulating predict.pyt

---
+++ predict.py
@@ -0,0 +1,10 @@
+from hello import add, multiply
+
+number1 = float("Type number 1")
+number2 = float("Type number 2")
+
+print(f"Adding {number1} + {number2}...")
+print(f"The result is: {add(number1, number2)}")
+
+print(f"Multiplying {number1} * {number2}...")
+print(f"The result is: {multiply(number1, number2)}")
